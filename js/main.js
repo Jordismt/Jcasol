776c7b68fffe1164145b40a5149b6cc0ed0f0c3a
Jordi Casanova <105922656+Jordismt@users.noreply.github.com>
2024-01-14 19:55:27 +0100

    Actualización portafolio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function toggleCard(element) {
+    element.classList.toggle('is-flipped');
+}
+
+function showList(element) {
+    element.classList.add('is-flipped');
+}
+
+function hideList(element) {
+    element.classList.remove('is-flipped');
+}
